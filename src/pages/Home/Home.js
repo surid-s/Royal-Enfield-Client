3975dbecfdffbddf0dc4fd8ac0ab512388089b56
surid-s <maruf15-9549@diu.edu.bd>
2021-11-10 17:55:58 +0600

    dependencies added

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Shared/Navigation/Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
